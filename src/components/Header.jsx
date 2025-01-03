ffaf04d146a54f5aa425e3e076ab74e9d77b0d3c
aesmith573 <aesmith.573@gmail.com>
2025-01-02 19:47:39 -0600

    Increase icon size using the fontSize prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import EditIcon from '@mui/icons-material/Edit';
 function Header() {
   return(<>
     <header className="todo-title">
-      <EditIcon className='edit-icon'/>
+      <EditIcon 
+        className='edit-icon'
+        fontSize='large' 
+      />
       <h1>Andy's Todo List!</h1>
     </header>
   </>)
