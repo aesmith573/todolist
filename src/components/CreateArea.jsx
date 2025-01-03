1f66e558c589c01a9f38c940b27ab794d1ce8af9
aesmith573 <aesmith.573@gmail.com>
2025-01-02 22:24:26 -0600

    Create submitNote function with callback

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,6 +2,9 @@ import AddIcon from "@mui/icons-material/Add";
 import { Fab } from "@mui/material";
 
 function CreateArea() {
+  function handleClick() {
+
+  }
   return(
     <div className="container">
         <form className="text-area">
@@ -13,7 +16,10 @@ function CreateArea() {
             placeholder="Note Content"
             rows="3"
           />
-          <Fab className="fab-button">
+          <Fab 
+            className="fab-button"
+            onClick={handleClick}
+          >
             <AddIcon />
           </Fab>
         </form>
