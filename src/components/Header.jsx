095b867fe48e3bff5317e23f6ef3d23d53433b3f
aesmith573 <aesmith.573@gmail.com>
2025-01-02 19:43:07 -0600

    Give EditIcon component a class name.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import EditIcon from '@mui/icons-material/Edit';
 function Header() {
   return(<>
     <header className="todo-title">
-      <EditIcon />
+      <EditIcon className='edit-icon'/>
       <h1>Andy's Todo List!</h1>
     </header>
   </>)
