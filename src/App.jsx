45f65dc280d69d1bba2ebe464600730808001533
aesmith573 <aesmith.573@gmail.com>
2025-01-02 19:57:54 -0600

    Import and implement CreateArea component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
+import CreateArea from "./components/CreateArea.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
+      <CreateArea />
     </>
   )
 }
