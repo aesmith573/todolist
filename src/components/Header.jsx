acd408363080f9a9528e0d9f4b5a66271ca6a57f
aesmith573 <aesmith.573@gmail.com>
2025-01-02 19:41:21 -0600

    Add EditIcon to Title.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import EditIcon from '@mui/icons-material/Edit';
 
 function Header() {
   return(<>
     <header className="todo-title">
+      <EditIcon />
       <h1>Andy's Todo List!</h1>
     </header>
   </>)
