f81026fcde10bf13a577523efd0ea46105d89557
aesmith573 <aesmith.573@gmail.com>
2025-01-05 00:18:11 -0600

    Use mui to style and build card template for Notes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Header from "./components/Header.jsx";
 import CreateArea from "./components/CreateArea.jsx";
 import Note from "./components/Note.jsx";
+import { useState } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote() {
+
+  }
 
   return (
     <>
       <Header />
       <CreateArea />
-      <Note />
+      <div className="card-container">
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </div>
     </>
   )
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,18 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 function Note() {
-  return(<><p>this is a note</p></>)
+  return(<div className='card'>
+    <Card>
+      <Typography variant='h5' component='div'>
+        Task Title
+      </Typography>
+      <CardContent>
+        I need to walk the dog. I want to walk them all around the neighborhood.
+      </CardContent>
+    </Card>
+  </div>)
 }
 
 export default Note;
